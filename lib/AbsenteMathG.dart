46530e807e841e5434acdf81b6d0709f37b394e3
ujju <ujju14321@gmail.com>
2024-03-18 18:39:42 +0530

    gst notes update

---
+++ lib/AbsenteMathG.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+
+class AbsenteMathG extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ChecklistPage(),
+    );
+  }
+}
+
+class ChecklistPage extends StatefulWidget {
+  @override
+  _ChecklistPageState createState() => _ChecklistPageState();
+}
+
+class _ChecklistPageState extends State<ChecklistPage> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  final List<String> students = List.generate(66, (index) => 'ROLL NO: ${index + 1}');
+  String searchText = '';
+  List<String> checkedStudents = [];
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> filteredStudents = students.where((student) {
+      return searchText.isEmpty || student.toLowerCase().contains(searchText.toLowerCase());
+    }).toList();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Student Checklist'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Search...',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (value) {
+                setState(() {
+                  searchText = value;
+                });
+              },
+            ),
+          ),
+          Expanded(
+            child: filteredStudents.isEmpty
+                ? Center(
+              child: Text('Sorry, no one in the list.'),
+            )
+                : Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: filteredStudents.length,
+                    itemBuilder: (context, index) {
+                      return StudentItem(
+                        student: filteredStudents[index],
+                        index: index + 1,
+                        onChanged: (isChecked) {
+                          if (isChecked) {
+                            checkedStudents.add(filteredStudents[index]);
+                          } else {
+                            checkedStudents.remove(filteredStudents[index]);
+                          }
+                        },
+                      );
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    // View absent students button pressed
+                    if (checkedStudents.isNotEmpty) {
+                      showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            title: Text('Absent Students'),
+                            content: SingleChildScrollView( // Wrap content in SingleChildScrollView
+                              child: Container(
+                                height: 400, // Increase the fixed height
+                                child: ListView(
+                                  children: [
+                                    for (var student in checkedStudents) Text(student, style: TextStyle(color: Colors.black, fontSize:14),),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            actions: [
+                              TextButton(
+                                onPressed: () {
+                                  Navigator.pop(context);
+                                },
+                                child: Text('Close'),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    } else {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text('No students are absent.'),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text('View Absent Students'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class StudentItem extends StatefulWidget {
+  final String student;
+  final int index;
+  final ValueChanged<bool> onChanged;
+
+  const StudentItem({Key? key, required this.student, required this.index, required this.onChanged}) : super(key: key);
+
+  @override
+  _StudentItemState createState() => _StudentItemState();
+}
+
+class _StudentItemState extends State<StudentItem> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+  bool isChecked = false;
+
+  Future<int> getPresent(String documentId) async {
+    try {
+      DocumentSnapshot snapshot = await _firestore
+          .collection('studentslist')
+          .doc(documentId)
+          .get();
+
+      int present = snapshot.get('presentMATHS2');
+      return present;
+    } catch (e) {
+      print('Error retrieving value from Firebase: $e');
+      return 0;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.symmetric(horizontal: 16.0),
+      title: Text(widget.student),
+      trailing: Checkbox(
+        value: isChecked,
+        onChanged: (value) async {
+          setState(() {
+            isChecked = value!;
+          });
+          // Update Firestore when the checkbox state changes
+          int presentValue = await getPresent('Rollno${widget.index}');
+          int newPresentValue = isChecked ? presentValue - 1 : presentValue + 1;
+          await updatePresentValue('Rollno${widget.index}', newPresentValue);
+          // Notify the parent widget about the change in checked state
+          widget.onChanged(isChecked);
+        },
+      ),
+    );
+  }
+
+  Future<void> updatePresentValue(String documentId, int newPresentValue) async {
+    try {
+      await _firestore.collection('studentslist').doc(documentId).update({
+        'presentMATHS2': newPresentValue,
+      });
+    } catch (e) {
+      print('Error updating present value in Firestore: $e');
+    }
+  }
+}
---
+++ lib/ViewCp-G.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sies_gst_notes/register.dart';
+import 'package:sies_gst_notes/FEsem1.dart';
+import 'package:sies_gst_notes/FEsem2.dart';
+import 'package:sies_gst_notes/SEsem1.dart';
+import 'package:sies_gst_notes/SEsem2.dart';
+import 'package:sies_gst_notes/TEsem1.dart';
+import 'package:sies_gst_notes/TEsem2.dart';
+import 'package:sies_gst_notes/Besem1.dart';
+import 'package:sies_gst_notes/BEsem2.dart';
+// import 'package:sies_gst_notes/addlec.dart';
+// import 'package:sies_gst_notes/AllStudentsListScreen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Student {
+  String name;
+  int present;
+
+  Student({required this.name, this.present = 0});
+}
+
+class Addabb {
+  List<Student> students;
+
+  Addabb({required this.students});
+}
+
+class ViewCpG extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StudentScreen(),
+    );
+  }
+}
+
+class StudentScreen extends StatefulWidget {
+  @override
+  _StudentScreenState createState() => _StudentScreenState();
+}
+
+class _StudentScreenState extends State<StudentScreen> {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<int> getLectures() async {
+    try {
+      DocumentSnapshot snapshot = await _firestore
+          .collection('myVariable')
+          .doc('GdivCP')
+          .get();
+
+      int total_lectures = snapshot.get('total_lectures');
+      return total_lectures;
+    } catch (e) {
+      print('Error retrieving value from Firebase: $e');
+      return 0;
+    }
+  }
+
+  Future<int> getPresent(String documentId) async {
+    try {
+      DocumentSnapshot snapshot =
+          await _firestore.collection('studentslist').doc(documentId).get();
+
+      int present = snapshot.get('presentCP');
+      return present;
+    } catch (e) {
+      print('Error retrieving value from Firebase: $e');
+      return 0;
+    }
+  }
+
+  int totalLectures = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    initializeStudentsPresentValues();
+  }
+
+  void initializeStudentsPresentValues() async {
+    for (var student in addab.students) {
+      int presentValue =
+          await getPresent('Rollno${addab.students.indexOf(student) + 1}');
+      student.present = presentValue;
+    }
+    totalLectures = await getLectures();
+    setState(() {});
+  }
+
+  Future<void> updatePresentValue(
+      String documentId, int newPresentValue) async {
+    try {
+      await _firestore.collection('studentslist').doc(documentId).update({
+        'presentCP': newPresentValue,
+      });
+    } catch (e) {
+      print('Error updating present value in Firestore: $e');
+    }
+  }
+
+  Addabb addab = Addabb(
+    students: List.generate(
+      66,
+      (index) => Student(name: (index + 1).toString()),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            color:
+                Color(0xFF222224), // Set the background color of the container
+            height: 150,
+            alignment: Alignment.center,
+            child: Text(
+              'Attendance List',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              color: Color(0xFFe1d5c9), // Set the background color of the page
+              child: ListView.builder(
+                itemCount: addab.students.length,
+                itemBuilder: (context, index) {
+                  var student = addab.students[index];
+                  return ListTile(
+                    title: RichText(
+                      text: TextSpan(
+                        style: TextStyle(color: Colors.black),
+                        children: [
+                          TextSpan(
+                            text: 'ROLL NO: ',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 19),
+                          ),
+                          TextSpan(
+                              text: '${student.name}',
+                              style: TextStyle(
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          TextSpan(
+                              text:
+                                  ' \n- Present: ${student.present}  Percentage: ${(student.present / totalLectures * 100).toStringAsFixed(2)}%',
+                              style: TextStyle(
+                                fontSize: 16,
+                              )),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
